3c1df5f5a97158235d6667988f6a0af27479340c
Andrew Deck <andrew.deck@triblio.com>
2019-06-04 16:02:35 -0400

    ignoring ties

--- cards.js
+++ cards.js
@@ -1,14 +1,42 @@
+const _ = require('lodash');
+const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
+function generateDeck() {
+  let deck = [];
+
+  for(let i = 0, iLen = suits.length; i<iLen; i++) {
+    for(let j = 0, jLen = values.length; j<jLen; j++) {
+      deck.push({
+        suit: suits[i],
+        value: values[j],
+        valueIndex: j
+      });
+    }
+  }
+  if(deck.length != 52) throw new Error('something went wrong in deck generation');
+  return deck;
+}
 
 function getShuffledDeck() {
-  
+  let deck = generateDeck();
+  return _.shuffle(deck);
 }
 
 module.exports.getShuffledDeck = getShuffledDeck;
 
-
 function dealCards(numPlayers, deck) {
+  let hands = [];
+  for(let i = 0; i<numPlayers; i++) {
+    hands.push([]);
+  }
 
+  for(let i = 0, len = deck.length; i<len; i++) {
+    let player = i % numPlayers;
+    let hand = hands[player];
+    hand.push(deck[i]);
+  }
+  return hands;
 } 
 
 module.exports.dealCards = dealCards;
--- index.js
+++ index.js
@@ -1,9 +1,48 @@
+const _ = require('lodash');
 const Cards = require('./cards.js');
 
 
 function playWar() {
+  let numberOfTurns = 0;
   let deck = Cards.getShuffledDeck();
+  let hands = Cards.dealCards(2, deck);
 
+  let playerOne = {
+    hand: hands[0],
+    pile: []
+  };
+  let playerTwo = {
+    hand: hands[1],
+    pile: []
+  }
+
+  while(playerOne.hand.length > 0 && playerTwo.hand.length > 0){
+    let playerOneCard = playerOne.hand.shift();
+    let playerTwoCard = playerTwo.hand.shift();
+
+    if(playerOneCard.valueIndex > playerTwoCard.valueIndex) {
+      playerOne.pile.push(playerOneCard, playerTwoCard);
+    } else if (playerTwoCard.valueIndex > playerOneCard.valueIndex) {
+      playerTwo.pile.push(playerOneCard, playerTwoCard);
+    }
+
+
+    shuffleIfHandIsEmpty(playerOne);
+    shuffleIfHandIsEmpty(playerTwo);
+    numberOfTurns++;
+    console.log(`P1: h ${playerOne.hand.length} p ${playerOne.pile.length} P2: h ${playerTwo.hand.length} p ${playerTwo.pile.length}`);
+  }
+  console.log(`The game took ${numberOfTurns} turns`);
+  return numberOfTurns;
+}
+
+function shuffleIfHandIsEmpty(player) {
+  let {hand, pile} = player;
+
+  if(hand.length === 0 && pile.length !== 0) {
+    player.hand = _.shuffle(pile);
+    player.pile = [];
+  }
 }
 
 
