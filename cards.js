322c90b4f9f99f6ddcfc58b5affa822d50d013f5
Andrew Deck <andrew.deck@triblio.com>
2019-06-04 10:14:23 -0400

    initial commit

---
+++ cards.js
@@ -0,0 +1,14 @@
+
+
+function getShuffledDeck() {
+  
+}
+
+module.exports.getShuffledDeck = getShuffledDeck;
+
+
+function dealCards(numPlayers, deck) {
+
+} 
+
+module.exports.dealCards = dealCards;
---
+++ index.js
@@ -0,0 +1,10 @@
+const Cards = require('./cards.js');
+
+
+function playWar() {
+  let deck = Cards.getShuffledDeck();
+
+}
+
+
+playWar();
